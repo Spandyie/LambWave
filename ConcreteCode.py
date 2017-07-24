518bfaf11ca9221dab59ba6da912cb45725ad0ef
Spandyie <smishra28@outlook.com>
2017-07-23 22:39:28 -0700

    Results

--- ConcreteCode.py
+++ ConcreteCode.py
@@ -1,94 +1,65 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 14 23:53:21 2017
+Created on Sat Jul 15 22:21:23 2017
 
 @author: Spandan_Mishra
 """
 
-def DynamicTimeWarping(x,y):
-    DTW={}
-    len1=len(x)
-    len2=len(y)
-    for i in range(len1):
-        DTW[(i,-1)]=float('inf')
-    
-    for j in range(len2):
-        DTW[(-1,j)]=float('inf')
-    DTW[(-1,-1)]=0
-    for i in range(len1):
-        for j in range(len2):
-            
-            DistTemp= (x[i]-y[j])**2
-            DTW[(i,j)]= DistTemp + min(DTW[(i-1,j-1)], DTW[(i-1,j)], DTW[(i,j-1)])
-    return(DTW[(len1-1,len2-1)])
-    
-    
-    
 from scipy.io import loadmat
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import glob
-#%matplotlib notebook
+import os
 
-data=[]
-TestData=[]
-FileNames= glob.glob("*.mat") #
-TestFileName=glob.glob("C:/Users/Spandan Mishra/Documents/LambWave/testData/*.mat")
-
-[data.append(loadmat(f,squeeze_me=True, struct_as_record=False)) for f in FileNames]  #loading all the data in the file
-[TestData.append(loadmat(f,squeeze_me=True, struct_as_record=False)) for f in TestFileName]
-#####################
-plt.plot(data[0]['a0'])
-plt.plot(data[0]['s0'])
-plt.plot(data[1]['s0'])
-plt.plot(data[2]['s0'])
-plt.plot(data[3]['s0'])
-plt.plot(data[4]['s0'])
-plt.xlabel('Sampling Number')
-plt.ylabel('Volts')
+NewTrainOne=[]
+NewTrainTwo=[]
+####################################
+NewTrainingData1=[]
+NewTrainingData2=[]
+def key_func(x):
+        return os.path.split(x)[-1]
+for data_file1,data_file2 in zip(sorted(os.listdir("Metal1/"),key=key_func),sorted(os.listdir("Metal2/"),key=key_func)):
+    NewTrainingData1.append(data_file1)
+    NewTrainingData2.append(data_file2)
 
 #########################################
-frequency=data[0]['setup'].signal_definition.frequency1
+folderPath="C:/Users/Spandan Mishra/Documents/GitHub/LambWave/Metal1"
+[NewTrainOne.append(loadmat(os.path.join(folderPath,f),squeeze_me=True, struct_as_record=False)) for f in NewTrainingData1]
+[NewTrainTwo.append(loadmat(os.path.join(folderPath,f),squeeze_me=True, struct_as_record=False)) for f in NewTrainingData2]
+###############################################
+frequency=NewTrainOne[4]['setup'].signal_definition.frequency1
 # sampling rate of the signal
-sampling_rate=data[0]['setup'].sampling_rate
-#################################################
-########################################### 
- 
+sampling_rate=NewTrainOne[0]['setup'].sampling_rate
+##################################################
 crosstalk=[]
 frequency=[]
 sampling_rate=[]
-for signals in data:
+for signals in NewTrainOne:
     frequency.append(signals['setup'].signal_definition.frequency1)
     sampling_rate.append(signals['setup'].sampling_rate)
     crosstalk.append(5/(signals['setup'].signal_definition.frequency1)* (signals['setup'].sampling_rate))
-    
-##################################################################################
-for i in range(5):
-    data[i]['s0'][1:int(crosstalk[i])]=0  #training data
-    TestData[i]['s0'][1:int(crosstalk[i])]=0  # test data
+######################################
+for i in range(25):
+    NewTrainOne[i]['s0'][1:int(crosstalk[i])]=0  #training data
+    NewTrainTwo[i]['s0'][1:int(crosstalk[i])]=0  # test data
     
 SensorData=list()
 ActuatorData=list()
 
-[SensorData.append(signal['s0']) for signal in data ]  # sensor data arranged in list
-[ActuatorData.append(signal['a0']) for signal in data]  # Actuator data arrange in list
-
+[SensorData.append(signal['s0']) for signal in NewTrainOne ]  # sensor data arranged in list
+[ActuatorData.append(signal['a0']) for signal in NewTrainOne]  # Actuator data arrange in list
+##############################################################################################################
 TestSensorData=list()
 TestActuatorData=list()
 
-[TestSensorData.append(signal['s0']) for signal in TestData ]  # Test data  for sensor  arranged in list
-[TestActuatorData.append(signal['a0']) for signal in TestData]  # test data for actuator  arrange in list
-   
-#####################
+[TestSensorData.append(signal['s0']) for signal in NewTrainTwo ]  # Test data  for sensor  arranged in list
+[TestActuatorData.append(signal['a0']) for signal in NewTrainTwo]  # test data for actuator  arrange in list
+###########################################################
 TotalData=SensorData+TestSensorData
 TotalData=np.asarray(TotalData)
-  
-########################
-#Caling Keras
-########################
-#from tensorflow.examples.tutorials.mnist import input_data
+#####################################
+#####################################
 from keras.layers import Input, Dense
 from keras.models import Model
 #from keras.optimizers import Adam
@@ -99,7 +70,7 @@ SensorData=np.asarray(SensorData) # converting list into array (training data)
 TestSensorData=  np.asarray(TestSensorData)
 InputSignal=SensorData[0]
 actual_signal_len=len(InputSignal)
-encoding_dim=100 # This going to be size of our encoded representation
+encoding_dim=500 # This going to be size of our encoded representation
 #this returns a tensor
 inputs = Input(shape=(actual_signal_len,))
 encoded=Dense(encoding_dim,activation='relu',activity_regularizer=regularizers.l1(10e-5))(inputs)
@@ -113,273 +84,88 @@ autoencoder= Model(input = inputs, output= decoded)
 #we'll configure our model to use a mean squarred error loss, and the Adam optimizer
 # we also train autoencoder for 50 epochs
 autoencoder.compile(optimizer='adam', loss='mse')
-autoencoder.fit(TotalData, TotalData, epochs=50, batch_size=1)
+autoencoder.fit(TotalData, TotalData, epochs=100, batch_size=1)
 
 #######################
 #Encode the images
 encoded_signal= encoder.predict(SensorData)
 EncodedTestData= encoder.predict(TestSensorData)
-"""
-In the above code we iterate over the testdata0 to testdata5 to estimate the 
-Dynamic time warping value all states with respect to the baseline signal. 
-Now a linear relationship between damage size and DTW distance will be used to 
-calibrate the damage size.This will enable us to quantify the size of the damage
- as well as its detection. We will set a threshold for the damage detection.
-
-"""
-from matplotlib.pyplot import cm
-c=cm.rainbow(np.linspace(0,1,5))
-labels=['no gap','0.2 mm gap','0.3 mm gap','0.4 mm gap','0.1 mm gap']
-#%matplotlib notebook
-plt.figure()
-for c,signal,l in zip(c,encoded_signal,labels):
-    plt.plot(signal,color=c,label=l)
-plt.xlabel('Sampling Number')
-plt.ylabel('Volts')
-plt.legend()
-
-###############################################
-c=cm.rainbow(np.linspace(0,1,5))
-labels=['no gap','0.2 mm gap','0.3 mm gap','0.4 mm gap','0.1 mm gap']
-#%matplotlib notebook
-plt.figure()
-for c,signal,l in zip(c,EncodedTestData,labels):
-    plt.plot(signal,color=c,label=l)
-
-plt.xlabel('Sampling Number')
-plt.ylabel('Volts')
-plt.legend()
-plt.title("Test Data")
 ################################
-# Calling DTW function
-DynamicWarp=[]
-DynamicWarpTest=[]
-[DynamicWarp.append(DynamicTimeWarping(encoded_signal[0],signal)) for signal in encoded_signal]
-[DynamicWarpTest.append(DynamicTimeWarping(EncodedTestData[0],signal)) for signal in EncodedTestData]
-gaps=[0,0.2,0.3,0.4,0.1]
+dist1=[]
+dist2=[]
+def mean(a):
+    return sum(a)/len(a)
+MeanBaseline=[]
+MeanBaselineTest=[]
+[MeanBaseline.append(i) for i in    map(mean,zip(*encoded_signal[0:4]))]
+[MeanBaselineTest.append(i) for i in    map(mean,zip(*EncodedTestData[0:4]))]
 
-#linear regression fucntion
-#
+for x,y in zip(encoded_signal,EncodedTestData):
+    dist1.append(np.linalg.norm(MeanBaseline-x))
+    dist2.append(np.linalg.norm(EncodedTestData[0]-y))
+############################################
+SelectedDist=[]
+SelectedDistTest=[]  # test data
+index=[0,5,10,15]
+[SelectedDist.append(dist1[i]) for i in index]
+[SelectedDistTest.append(dist2[i]) for i in index]
+###############################################
 from sklearn import linear_model
-#from sklearn import preprocessing
+from math import log
+from numpy import exp
+gaps=[0.00001,0.1,0.2,0.3]
+logGap=[]
+[logGap.append(log(i))  for i in gaps]
+SelectedDist=np.asarray(SelectedDist)
+logGap=np.asarray(logGap)
 
-#DTWScaled=preprocessing.scale(DynamicWarp)
-
-#DTWScaled=np.asarray(DTWScaled)
-DynamicWarp=np.asarray(DynamicWarp)
-gaps=np.asarray(gaps)
-#lets create a regression object
-regr= linear_model.LinearRegression(fit_intercept=True,normalize=True)
-#fitting the linear regression
-
-regr.fit(DynamicWarp.reshape(5,1),gaps.reshape(5,1))
-print("The regression coefficients are as:[%.7f, %.7f]" % (regr.intercept_ ,  regr.coef_))
-# The final prediction of the test data
-DTWtest=np.asarray(DynamicWarpTest)
-###
-TrainingGaps=regr.predict(DynamicWarp.reshape(5,1))
-PredictionGaps=regr.predict(DTWtest.reshape(5,1))
-##########################
-
+expRegr=linear_model.LinearRegression(fit_intercept=True,normalize=True)
+expRegr.fit(SelectedDist.reshape(4,1),logGap.reshape(4,1))
+print("The regression coefficients are as:[%.7f, %.7f]" % (expRegr.intercept_ ,  expRegr.coef_))
+TrainingGapsExp=expRegr.predict(np.asarray(SelectedDist).reshape(4,1))
+#######################################################
+testData=np.asarray(dist1)
+Pred=expRegr.predict(testData[:-5].reshape(20,1)) # predeiction of the Metal 1
+PredictedGap=exp(Pred)
+print(PredictedGap)
+#######################################################
+x=[0.00001,0.1,0.2,0.3]
+GapVec=[]
+[GapVec.append(np.tile(i,(1,5))) for i in x]
 plt.figure()
-plt.scatter(gaps,PredictionGaps, c='b', marker="s")
-plt.xlabel('Actual Gap')
-plt.ylabel('predicted Gaps')
+plt.scatter(SelectedDist,gaps,color='blue',label='Data used to Train')
+plt.plot(SelectedDist,exp(TrainingGapsExp),color='red',label='Prediction on Training data')
+plt.scatter(testData[:-5],PredictedGap,color='green',label='Prediction on Metal 1 Data')
+plt.ylabel('Gaps (mm)')
+plt.xlabel('Damage Index')
+plt.legend()
+plt.show()
+#####################################
+d=[0,0.1,0.2,0.3,0.4]
+plt.figure()
+plt.scatter(GapVec,PredictedGap)
+plt.plot(d,d,color='red')
+plt.xlabel('Actual Damage Size')
+plt.ylabel('Predicted Damage Size')
 plt.show()
 
+###################
+##prediction error
+GapVecNew = np.concatenate([np.array(i[0]) for i in GapVec])  # convert gapvec in numpy
 
+error=np.sum(np.square(np.subtract(GapVecNew,PredictedGap)))
 
+### Data from Metal 2##########
+xMetal2=[0.00001,0.1,0.2,0.3,0.4]
+GapVecMetal2=[]
+[GapVecMetal2.append(np.tile(i,(1,5))) for i in xMetal2]
+testDataMetal2=np.asarray(dist2)
+PredictedValueMetal2=expRegr.predict(testDataMetal2.reshape(25,1))
+print(exp(PredictedValueMetal2))
 
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 14 23:53:21 2017
-
-@author: Spandan_Mishra
-"""
-
-def DynamicTimeWarping(x,y):
-    DTW={}
-    len1=len(x)
-    len2=len(y)
-    for i in range(len1):
-        DTW[(i,-1)]=float('inf')
-    
-    for j in range(len2):
-        DTW[(-1,j)]=float('inf')
-    DTW[(-1,-1)]=0
-    for i in range(len1):
-        for j in range(len2):
-            
-            DistTemp= (x[i]-y[j])**2
-            DTW[(i,j)]= DistTemp + min(DTW[(i-1,j-1)], DTW[(i-1,j)], DTW[(i,j-1)])
-    return(DTW[(len1-1,len2-1)])
-    
-    
-    
-from scipy.io import loadmat
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import glob
-#%matplotlib notebook
-
-data=[]
-TestData=[]
-FileNames= glob.glob("*.mat") #
-TestFileName=glob.glob("C:/Users/Spandan Mishra/Documents/LambWave/testData/*.mat")
-
-[data.append(loadmat(f,squeeze_me=True, struct_as_record=False)) for f in FileNames]  #loading all the data in the file
-[TestData.append(loadmat(f,squeeze_me=True, struct_as_record=False)) for f in TestFileName]
-#####################
-plt.plot(data[0]['a0'])
-plt.plot(data[0]['s0'])
-plt.plot(data[1]['s0'])
-plt.plot(data[2]['s0'])
-plt.plot(data[3]['s0'])
-plt.plot(data[4]['s0'])
-plt.xlabel('Sampling Number')
-plt.ylabel('Volts')
-
-#########################################
-frequency=data[0]['setup'].signal_definition.frequency1
-# sampling rate of the signal
-sampling_rate=data[0]['setup'].sampling_rate
-#################################################
-########################################### 
- 
-crosstalk=[]
-frequency=[]
-sampling_rate=[]
-for signals in data:
-    frequency.append(signals['setup'].signal_definition.frequency1)
-    sampling_rate.append(signals['setup'].sampling_rate)
-    crosstalk.append(5/(signals['setup'].signal_definition.frequency1)* (signals['setup'].sampling_rate))
-    
-##################################################################################
-for i in range(5):
-    data[i]['s0'][1:int(crosstalk[i])]=0  #training data
-    TestData[i]['s0'][1:int(crosstalk[i])]=0  # test data
-    
-SensorData=list()
-ActuatorData=list()
-
-[SensorData.append(signal['s0']) for signal in data ]  # sensor data arranged in list
-[ActuatorData.append(signal['a0']) for signal in data]  # Actuator data arrange in list
-
-TestSensorData=list()
-TestActuatorData=list()
-
-[TestSensorData.append(signal['s0']) for signal in TestData ]  # Test data  for sensor  arranged in list
-[TestActuatorData.append(signal['a0']) for signal in TestData]  # test data for actuator  arrange in list
-   
-#####################
-TotalData=SensorData+TestSensorData
-TotalData=np.asarray(TotalData)
-  
-########################
-#Caling Keras
-########################
-#from tensorflow.examples.tutorials.mnist import input_data
-from keras.layers import Input, Dense
-from keras.models import Model
-#from keras.optimizers import Adam
-from keras import regularizers
-
-np.random.seed(7)
-SensorData=np.asarray(SensorData) # converting list into array (training data)
-TestSensorData=  np.asarray(TestSensorData)
-InputSignal=SensorData[0]
-actual_signal_len=len(InputSignal)
-encoding_dim=100 # This going to be size of our encoded representation
-#this returns a tensor
-inputs = Input(shape=(actual_signal_len,))
-encoded=Dense(encoding_dim,activation='relu',activity_regularizer=regularizers.l1(10e-5))(inputs)
-decoded=Dense(actual_signal_len)(encoded)
-
-# this model maps an input to its encoded representation
-encoder = Model(input=inputs, output=encoded)
-
-## this model maps an input to its reconstruction
-autoencoder= Model(input = inputs, output= decoded)
-#we'll configure our model to use a mean squarred error loss, and the Adam optimizer
-# we also train autoencoder for 50 epochs
-autoencoder.compile(optimizer='adam', loss='mse')
-autoencoder.fit(TotalData, TotalData, epochs=50, batch_size=1)
-
-#######################
-#Encode the images
-encoded_signal= encoder.predict(SensorData)
-EncodedTestData= encoder.predict(TestSensorData)
-"""
-In the above code we iterate over the testdata0 to testdata5 to estimate the 
-Dynamic time warping value all states with respect to the baseline signal. 
-Now a linear relationship between damage size and DTW distance will be used to 
-calibrate the damage size.This will enable us to quantify the size of the damage
- as well as its detection. We will set a threshold for the damage detection.
-
-"""
-from matplotlib.pyplot import cm
-c=cm.rainbow(np.linspace(0,1,5))
-labels=['no gap','0.2 mm gap','0.3 mm gap','0.4 mm gap','0.1 mm gap']
-#%matplotlib notebook
 plt.figure()
-for c,signal,l in zip(c,encoded_signal,labels):
-    plt.plot(signal,color=c,label=l)
-plt.xlabel('Sampling Number')
-plt.ylabel('Volts')
+plt.scatter(GapVecMetal2,exp(PredictedValueMetal2),color='red',label='Prediction on Metal 2 data')
+plt.ylabel('Gaps (mm)')
+plt.xlabel('Damage Index')
 plt.legend()
-
-###############################################
-c=cm.rainbow(np.linspace(0,1,5))
-labels=['no gap','0.2 mm gap','0.3 mm gap','0.4 mm gap','0.1 mm gap']
-#%matplotlib notebook
-plt.figure()
-for c,signal,l in zip(c,EncodedTestData,labels):
-    plt.plot(signal,color=c,label=l)
-
-plt.xlabel('Sampling Number')
-plt.ylabel('Volts')
-plt.legend()
-plt.title("Test Data")
-################################
-# Calling DTW function
-DynamicWarp=[]
-DynamicWarpTest=[]
-[DynamicWarp.append(DynamicTimeWarping(encoded_signal[0],signal)) for signal in encoded_signal]
-[DynamicWarpTest.append(DynamicTimeWarping(EncodedTestData[0],signal)) for signal in EncodedTestData]
-gaps=[0,0.2,0.3,0.4,0.1]
-
-#linear regression fucntion
-#
-from sklearn import linear_model
-#from sklearn import preprocessing
-
-#DTWScaled=preprocessing.scale(DynamicWarp)
-
-#DTWScaled=np.asarray(DTWScaled)
-DynamicWarp=np.asarray(DynamicWarp)
-gaps=np.asarray(gaps)
-#lets create a regression object
-regr= linear_model.LinearRegression(fit_intercept=True,normalize=True)
-#fitting the linear regression
-
-regr.fit(DynamicWarp.reshape(5,1),gaps.reshape(5,1))
-print("The regression coefficients are as:[%.7f, %.7f]" % (regr.intercept_ ,  regr.coef_))
-# The final prediction of the test data
-DTWtest=np.asarray(DynamicWarpTest)
-###
-TrainingGaps=regr.predict(DynamicWarp.reshape(5,1))
-PredictionGaps=regr.predict(DTWtest.reshape(5,1))
-##########################
-
-plt.figure()
-plt.scatter(gaps,PredictionGaps, c='b', marker="s")
-plt.xlabel('Actual Gap')
-plt.ylabel('predicted Gaps')
 plt.show()
-
-
-
-
->>>>>>> origin/master
